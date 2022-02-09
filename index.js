dbb294c558ef936143fba09cf900329473e2634d
Denis <denis@releaseband.com>
2022-02-09 12:31:14 +0300

    fix: add plugin:react/jsx-runtime in extends

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 module.exports = {
-  extends: ['@releaseband/eslint-config-typescript', 'airbnb', 'airbnb/hooks', 'airbnb-typescript'],
+  extends: [
+    '@releaseband/eslint-config-typescript',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/jsx-runtime',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
 };
